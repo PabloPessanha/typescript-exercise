cba3d7921d1d52a09243937eddfcdf456a776909
Pablo Pessanha <pablopessanh@gmail.com>
2021-07-01 23:14:21 -0300

    exercise 3 test add

---
+++ typescript-exercises/tests/exercise3.test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import { FirstArgument, IsTypeEqual, typeAssert } from '../type-assertions';
+import { logPerson, Person, persons } from '../exercise3';
+
+typeAssert<
+    IsTypeEqual<
+        Person,
+        {name: string; age: number} &({occupation: string} | {role: string})
+    >
+>();
+
+typeAssert<
+    IsTypeEqual<
+        typeof persons,({name: string; age: number} & ({occupation: string} | {role: string}))[]
+    >
+>();
+
+typeAssert<
+    IsTypeEqual<
+        FirstArgument<typeof logPerson>,
+        {name: string; age: number} &({occupation: string} | {role: string})
+    >
+>();
+
+typeAssert<
+    IsTypeEqual<
+        ReturnType<typeof logPerson>,
+        void
+    >
+>();
